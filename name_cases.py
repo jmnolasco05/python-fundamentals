66f1a7af311e7005f8527b97387114a07383b30f
Joan Nolasco <joan.nolasco.castillo@gmail.com>
2025-03-30 15:09:32 -0400

    Add string case exercice

---
+++ name_cases.py
@@ -0,0 +1,23 @@
+name = 'Eric'
+print(f'Hello, {name}, would you like to learn some Python today?')
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+quote = 'A person who never made a mistake never tried anything new.'
+print(f'Albert Einstein once said, "{quote}"')
+
+famous_person = 'Albert Einstein'
+message = f'{famous_person} once said, "{quote}"'
+print(message)
+
+name_with_spaces = ' \tEric Antonio \nFernandez '
+print(name_with_spaces)
+print(name_with_spaces.lstrip())
+print(name_with_spaces.rstrip())
+print(name_with_spaces.strip())
+
+
+file_name = 'python_notes.txt'
+print(file_name.removesuffix('.txt'))
